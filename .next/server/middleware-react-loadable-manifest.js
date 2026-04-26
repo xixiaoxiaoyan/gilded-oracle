09e1bdfbe751258aa8f8997047ba3e1bc025ea04
喜 <xi@xideMacBook.local>
2026-04-26 12:43:03 +0800

    Add .gitignore

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/page.tsx -> @/components/OracleDeck\":{\"id\":\"app/page.tsx -> @/components/OracleDeck\",\"files\":[\"static/chunks/_app-pages-browser_src_components_OracleDeck_tsx.js\"]}}"
